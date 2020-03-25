e798bdb5cbdf40d9349ab65a05c7fc2006213b17
gonghonglou <gonghonglou@icloud.com>
2020-03-26 01:19:06 +0800

    check block exist or not

--- HoloTableViewMGPlugin/Classes/MGProxy/HoloTableRowMGProxy.m
+++ HoloTableViewMGPlugin/Classes/MGProxy/HoloTableRowMGProxy.m
@@ -11,31 +11,53 @@
 @implementation HoloTableRowMGProxy
 
 - (BOOL)swipeTableCell:(MGSwipeTableCell *)cell canSwipe:(MGSwipeDirection)direction fromPoint:(CGPoint)point {
-    return self.action.canSwipeHandler(cell, direction, point);
+    if (self.action.canSwipeHandler) {
+        return self.action.canSwipeHandler(cell, direction, point);
+    } else {
+        return YES;
+    }
 }
 
 - (void)swipeTableCell:(MGSwipeTableCell *)cell didChangeSwipeState:(MGSwipeState)state gestureIsActive:(BOOL)gestureIsActive {
-    self.action.didChangeSwipeHandler(cell, state, gestureIsActive);
+    if (self.action.didChangeSwipeHandler) {
+        self.action.didChangeSwipeHandler(cell, state, gestureIsActive);
+    }
 }
 
 - (BOOL)swipeTableCell:(MGSwipeTableCell *)cell tappedButtonAtIndex:(NSInteger)index direction:(MGSwipeDirection)direction fromExpansion:(BOOL)fromExpansion {
-    return self.action.tappedSwipeButtonHandler(cell, index, direction, fromExpansion);
+    if (self.action.tappedSwipeButtonHandler) {
+        return self.action.tappedSwipeButtonHandler(cell, index, direction, fromExpansion);
+    } else {
+        return NO;
+    }
 }
 
 - (NSArray<UIView *> *)swipeTableCell:(MGSwipeTableCell *)cell swipeButtonsForDirection:(MGSwipeDirection)direction swipeSettings:(MGSwipeSettings *)swipeSettings expansionSettings:(MGSwipeExpansionSettings *)expansionSettings {
-    return self.action.swipeButtonsHandler(cell, direction, swipeSettings, expansionSettings);
+    if (self.action.swipeButtonsHandler) {
+        return self.action.swipeButtonsHandler(cell, direction, swipeSettings, expansionSettings);
+    } else {
+        return [NSArray array];
+    }
 }
 
 - (BOOL)swipeTableCell:(MGSwipeTableCell *)cell shouldHideSwipeOnTap:(CGPoint)point {
-    return self.action.shouldHideSwipeOnTapHandler(cell, point);
+    if (self.action.shouldHideSwipeOnTapHandler) {
+        return self.action.shouldHideSwipeOnTapHandler(cell, point);
+    } else {
+        return YES;
+    }
 }
 
 - (void)swipeTableCellWillBeginSwiping:(MGSwipeTableCell *)cell {
-    self.action.willBeginSwipingHandler(cell);
+    if (self.action.willBeginSwipingHandler) {
+        self.action.willBeginSwipingHandler(cell);
+    }
 }
 
 - (void)swipeTableCellWillEndSwiping:(MGSwipeTableCell *)cell {
-    self.action.willEndSwipingHandler(cell);
+    if (self.action.willEndSwipingHandler) {
+        self.action.willEndSwipingHandler(cell);
+    }
 }
 
 #pragma mark getter
