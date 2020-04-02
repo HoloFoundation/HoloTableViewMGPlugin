9f4e6cebf0e5d3ecd9a824bf7e2f92f22486ed50
gonghonglou <gonghonglou@icloud.com>
2020-04-02 23:44:00 +0800

    implement swip button maker

--- HoloTableViewMGPlugin/Classes/MGPlugin/HoloTableRowMGMaker.m
+++ HoloTableViewMGPlugin/Classes/MGPlugin/HoloTableRowMGMaker.m
@@ -18,9 +18,80 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        self.swipeButton = [MGSwipeButton new];
+        self.swipeButton = [MGSwipeButton buttonWithType:UIButtonTypeCustom];
+        self.swipeButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        self.swipeButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+        [self.swipeButton setEdgeInsets:UIEdgeInsetsMake(0, 10, 0, 10)];
     }
     return self;
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(NSString *))title {
+    return ^(id obj) {
+        [self.swipeButton setTitle:obj forState:UIControlStateNormal];
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(UIImage *))icon {
+    return ^(id obj) {
+        [self.swipeButton setImage:obj forState:UIControlStateNormal];
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(UIColor *))backgroundColor {
+    return ^(id obj) {
+        self.swipeButton.backgroundColor = obj;
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(UIEdgeInsets))insets {
+    return ^(UIEdgeInsets insets) {
+        [self.swipeButton setEdgeInsets:insets];
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(MGSwipeButtonCallback))callback {
+    return ^(MGSwipeButtonCallback callback) {
+        self.swipeButton.callback = callback;
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(CGFloat))buttonWidth {
+    return ^(CGFloat f) {
+        self.swipeButton.buttonWidth = f;
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(CGFloat))padding {
+    return ^(CGFloat f) {
+        [self.swipeButton setPadding:f];
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker *)centerIconOverText {
+    [self.swipeButton centerIconOverText];
+    return self;
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(CGFloat))centerIconOverTextSpacing {
+    return ^(CGFloat f) {
+        [self.swipeButton centerIconOverTextWithSpacing:f];
+        return self;
+    };
+}
+
+- (HoloTableRowMGSwipeButtonMaker * (^)(UIColor *))tintColor {
+    return ^(id obj) {
+        [self.swipeButton iconTintColor:obj];
+        return self;
+    };
 }
 
 @end
